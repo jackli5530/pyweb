95d7fb1397938d458bfa672fc4055a8152b2ce12
jackli <jackli5530@126.com>
2017-02-22 09:15:54 +0800

    commit kali

--- src/myindex/bb.py
+++ src/myindex/bb.py
@@ -1,5 +1,17 @@
-from anaconda_navigator.utils.py3compat import request
+#coding=utf-8
 from django.shortcuts import render_to_response
 
 def hi(request):
-    return render_to_response("index.html")
+    dataset={"result":"必须输入数字"}
+    if "mustnum" in request.POST and isMyNumber(request.POST.get("mustnum")):
+        dataset["result"]="输入正确"
+    else:
+        dataset["result"]="输入不正确"
+    
+    return render_to_response("index.html",dataset)
+
+def isMyNumber(num):
+    if num.isnumeric() and int(num)>1:
+        return True
+    else:
+        return False
